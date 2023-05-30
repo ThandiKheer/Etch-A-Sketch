2ae68fe2d3e049d24f974c332a072239c0ef113b
AnmolMehta <anmolmehta162@gmail.com>
2023-05-30 19:29:47 +0530

    Change color of newDivs on hover, create erase Button.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /*----------------------------------------------------- */
 const handleButton1 = document.querySelector(".btn1");
 const handleButton2 = document.querySelector(".btn2");
+const handleButton3 = document.querySelector(".eraseBtn");
 
 const divContainer = document.querySelector(".divContainer");
 
@@ -17,14 +18,21 @@ function handleButtonEvent1(){
     let n = 16;
     for( let i = 0; i < n*n; i++){
         const createDiv = document.createElement("div");
-        createDiv.style.border = "solid black 1px";
         createDiv.classList.add("newDiv");
         createDiv.style.height = `${100/n}%`;
         createDiv.style.width = `${100/n}%`;
         createDiv.style.boxSizing = "border-Box";
         gridContainer.appendChild(createDiv);
+        createDiv.addEventListener("mouseover" , () => {
+            createDiv.style.backgroundColor = "black";
+        });
+        handleButton3.addEventListener('click' , ()=>{
+            createDiv.addEventListener('mouseover' , ()=>{
+                createDiv.style.backgroundColor = "";
+            });
+        });
     }
-    divContainer.appendChild(gridContainer); 
+    divContainer.appendChild(gridContainer);
 }
 
 function handleButtonEvent2(){
@@ -39,13 +47,22 @@ function handleButtonEvent2(){
     for( let i = 0; i < n*n; i++){
         const createDiv = document.createElement("div");
         createDiv.classList.add("newDiv");
-        createDiv.style.border = "solid black 1px";
         createDiv.style.height = `${100/n}%`;
         createDiv.style.width = `${100/n}%`;
         createDiv.style.boxSizing = "border-Box";
         gridContainer.appendChild(createDiv);
+        createDiv.addEventListener("mouseover" , () => {
+            createDiv.style.backgroundColor = "black";
+        });
+        handleButton3.addEventListener('click' , ()=>{
+            
+            createDiv.addEventListener('mouseover' , ()=>{
+                createDiv.style.backgroundColor = "";
+            });
+        });
     }
     divContainer.appendChild(gridContainer); 
+    
 };
 
 /*---------------------------------------------------------- */
